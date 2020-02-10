8912a5d096c337a8e8b6f674540f8732612f35d3
Chris <mr.skytte@me.com>
2020-02-10 22:19:52 +0100

    Added more line, and is able to write them all at once but can't get it to write one line at a time

--- typewriter.js
+++ typewriter.js
@@ -1,57 +1,57 @@
 "use strict";
-window.addEventListener("DOMContentLoaded", activateBtn);
+// window.addEventListener("DOMContentLoaded", init);
 
-// Define elements
+setInterval(function() {
+  // Define elements
+  let texts = document.querySelectorAll(".typewritten");
+  texts.forEach(function(oneText) {
+    let text = oneText.textContent;
+    text = text.trim();
+    let len = text.length;
+    let e = 0;
+    let newString = "";
+    let button = document.querySelector(".button");
+    oneText.textContent = "";
+    let eachCut;
+    button.addEventListener("click", startTyping);
 
-let text = document.querySelector(".typewritten").textContent;
-text = text.trim();
-let len = text.length;
-let e = 0;
-let newString = "";
-let button = document.querySelector(".button");
-document.querySelector(".typewritten").textContent = "";
-let eachCut;
-function activateBtn() {
-  button.addEventListener("click", startTyping);
-}
+    // Define audio
 
-// Define audio
+    const typekey1 = document.querySelector("#typekey1");
+    const typekey2 = document.querySelector("#typekey2");
+    const typespace = document.querySelector("#typespace");
+    const typelast = document.querySelector("#typelast");
+    const typereturn = document.querySelector("#typereturn");
 
-const typekey1 = document.querySelector("#typekey1");
-const typekey2 = document.querySelector("#typekey2");
-const typespace = document.querySelector("#typespace");
-const typelast = document.querySelector("#typelast");
-const typereturn = document.querySelector("#typereturn");
+    // Audio array for random keysound
 
-// Audio array for random keysound
+    let typeArr = [typekey1, typekey2];
 
-let typeArr = [typekey1, typekey2];
+    // Cut string into individual bits and put them together
+    function startTyping() {
+      button.removeEventListener("click", startTyping);
+      eachCut = setInterval(cutTheString, 200);
+    }
 
-// Cut string into individual bits and put them together
-function startTyping() {
-  button.removeEventListener("click", startTyping);
-  eachCut = setInterval(cutTheString, 200);
-}
-
-function cutTheString() {
-  if (e + 1 == len) {
-    clearInterval(eachCut);
-  }
-  // Figure out whether the character is a space or any other character
-  if (text[e] == " ") {
-    typespace.play();
-  } else {
-    let sound = Math.floor(Math.random() * 2);
-    typeArr[sound].play();
-  }
-  newString = newString + text[e];
-  console.log(newString);
-  document.querySelector(".typewritten").textContent = newString;
-  e++;
-}
-
-// Play sound according to the character
-
+    function cutTheString() {
+      if (e + 1 == len) {
+        clearInterval(eachCut);
+      }
+      // Figure out whether the character is a space or any other character
+      if (text[e] == " ") {
+        // Play sound according to the character
+        typespace.play();
+      } else {
+        let sound = Math.floor(Math.random() * 2);
+        typeArr[sound].play();
+      }
+      newString = newString + text[e];
+      console.log(newString);
+      oneText.textContent = newString;
+      e++;
+    }
+  });
+}, 3000);
 // Find each .typewritten class and apply above steps one at a time
 
 // Find <br> in string
